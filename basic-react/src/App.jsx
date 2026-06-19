2f1fbf867c27b1b277b37b1c85555921fd6d7f04
Sri Vardhan Bolli <bollisrivardhan8@gmail.com>
2026-06-19 15:59:44 +0530

    add tailwind CSS

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App=()=>{
   return(
     <div>
-      <p>Hello World!</p>
+      <p className="p-10">Hello World!</p>
       <h1>this is my react folder</h1>
     </div>
   )
